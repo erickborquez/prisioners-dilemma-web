ce697e8c4ad3ed19c45cbdf551587b212957da28
Erick Borquez <erickborquez@gmail.com>
2024-02-19 23:55:59 -0600

    Artificial delay

--- src/game/strategies/AlwaysSplit.ts
+++ src/game/strategies/AlwaysSplit.ts
@@ -4,6 +4,9 @@ import { GameActorStrategy } from "./ActorStrategy";
 // ********************************************************************************
 export class GameActorStrategyAlwaysSplit extends GameActorStrategy {
   protected async createAction(state: GameState): Promise<GameAction> {
+    // artificial delay
+    await new Promise(resolve => setTimeout(resolve, Math.random() * 2000));
+
     // ignore the state and always split
     return { actorId: this.actor.id, action: GameActionType.Split };
   }
--- src/game/strategies/AlwaysTake.ts
+++ src/game/strategies/AlwaysTake.ts
@@ -4,6 +4,9 @@ import { GameActorStrategy } from "./ActorStrategy";
 // ********************************************************************************
 export class GameActorStrategyAlwaysTake extends GameActorStrategy {
   protected async createAction(state: GameState): Promise<GameAction> {
+    // artificial delay
+    await new Promise(resolve => setTimeout(resolve, Math.random() * 2000));
+    
     // ignore the state and always take
     return { actorId: this.actor.id, action: GameActionType.Take };
   }
