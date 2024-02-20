0601bc70ca3ffe95d02126aace19490e62c877b4
Erick Borquez <erickborquez@gmail.com>
2024-02-19 23:52:53 -0600

    Define ActorStrategy

---
+++ src/game/strategies/ActorStrategy.ts
@@ -0,0 +1,31 @@
+import { ActorStrategy } from "@/actor/type";
+
+import { GameAction, GameState, GameStatus } from "../type";
+
+// ********************************************************************************
+export interface IGameActorStrategy {
+  /** the actor following this strategy */
+  actor: ActorStrategy;
+
+  /** creates an Action from the current state for the actor following this strategy */
+  requestAction(state: GameState): Promise<GameAction>;
+}
+
+export abstract class GameActorStrategy {
+  // == Lifecycle =================================================================
+  constructor(public readonly actor: ActorStrategy) {/*nothing else*/}
+
+  // == Public API ================================================================
+  /** creates an Action from the current state for the actor following this strategy */
+  public requestAction(state: GameState){
+    // validate the state
+    if(state.status !== GameStatus.InProgress) throw new Error(`Actor (${this.actor.id}) is not allowed to play in the current state: ${state.status}`);
+    if(state.actorId !== this.actor.id) throw new Error(`The actor ${this.actor.id} is not allowed to play`);
+
+    // create the action
+    return this.createAction(state);
+  }
+
+  // ==============================================================================
+  protected abstract createAction(state: GameState): Promise<GameAction>
+}
