9b897478af66ed32d9dd4a7a0703e64070c86792
Erick Borquez <erickborquez@gmail.com>
2024-02-22 21:48:08 -0600

    Consistency

--- src/game/strategies/ProbablyTakes.ts
+++ src/game/strategies/ProbablyTakes.ts
@@ -5,12 +5,12 @@ import { GameActorStrategy } from "./ActorStrategy";
 
 // ********************************************************************************
 export class GameActorStrategyProbablyTake extends GameActorStrategy {
-    protected async createAction(state: GameState): Promise<GameAction> {
-        // artificial delay
-        await new Promise(resolve => setTimeout(resolve, Math.random() * 800));
+  protected async createAction(state: GameState): Promise<GameAction> {
+    // artificial delay
+    await new Promise(resolve => setTimeout(resolve, Math.random() * 800));
 
-        // ignore the state and takes with 'p' probability
-        const probability: number = 0.7;
-        return { actorId: this.actor.id, action: randomChoice(probability)};
-    }
+    // ignore the state and takes with 'p' probability
+    const probability: number = 0.7;
+    return { actorId: this.actor.id, action: randomChoice(probability)};
+  }
 }
--- src/util/functions.ts
+++ src/util/functions.ts
@@ -4,9 +4,6 @@ import { GameActionType } from "@/game/type";
 // Returns a random choice
 export function randomChoice(probability: number): GameActionType {
   const randomChance = Math.random();
-  if(randomChance <= probability){
-    return GameActionType.Take;
-  } else {
-    return GameActionType.Split;
-  }
+  if(randomChance <= probability) return GameActionType.Take;
+  else return GameActionType.Split;
 }
